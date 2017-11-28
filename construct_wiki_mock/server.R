f1af39811ce12b109fc1d9d1009f6a030f4d640b
Daniel Chen <Maveri2k@yahoo.com>
2017-11-27 22:19:13 -0500

    move data load into global.R

--- construct_wiki_mock/server.R
+++ construct_wiki_mock/server.R
@@ -1,13 +1,4 @@
 library(shiny)
-library(data.tree)
-library(networkD3)
-library(DT)
-
-pokemon <- read.csv('data/pokemon.csv', sep = '\t')
-
-tree <- FromDataFrameNetwork(pokemon[, 1:2])
-lol <- ToListExplicit(tree, unname = TRUE)
-
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
