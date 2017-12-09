140ccd77ff955db79cda8d2bdb8558a19b24e70a
Daniel Chen <Maveri2k@yahoo.com>
2017-12-09 00:54:56 +0000

    make it shiny

--- shiny/server.R
+++ shiny/server.R
@@ -3,18 +3,22 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-  output$radial_network <- renderRadialNetwork({
-      radialNetwork(lol)
-  })
+    output$vis_network <- renderVisNetwork({
+        visNetwork(nodes, edge_list, width = "100%", height = "800px") %>%
+            visEdges(arrows = "from") %>%
+            visOptions(manipulation = TRUE,
+                       collapse = TRUE) %>%
+            visLayout(randomSeed = 42)
+    })
 
 
-  output$construct_dt <- DT::renderDataTable({
-      pokemon
-  })
+    output$construct_dt <- DT::renderDataTable({
+        pokemon
+    })
 
-  output$construct_definition <- renderText({
-      row_dat <- pokemon[pokemon$name == input$construct_name, ]
-      sprintf("Construct: %s\nEmbedded under: %s\nDefinition:%s",
-              row_dat$name, row_dat$meta.contruct, row_dat$definition)
-  })
+    output$construct_definition <- renderText({
+        row_dat <- pokemon[pokemon$name == input$construct_name, ]
+        sprintf("Construct: %s\nEmbedded under: %s\nDefinition:%s",
+                row_dat$name, row_dat$meta.contruct, row_dat$definition)
+    })
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -2,7 +2,7 @@ library(shiny)
 
 shinyUI(navbarPage("Construct Viewer",
                    tabPanel("Network",
-                            radialNetworkOutput("radial_network")),
+                            visNetworkOutput("vis_network", width = "100%", height = "800px")),
                    tabPanel("Definitions",
                             selectizeInput('construct_name', 'Construct Name', pokemon$name),
                             verbatimTextOutput('construct_definition')
