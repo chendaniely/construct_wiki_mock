37bf997d316083231c788d51c500676e89e88b28
Daniel Chen <Maveri2k@yahoo.com>
2017-12-13 19:48:04 +0000

    fluid row the graph with the notes on side

--- shiny/ui.R
+++ shiny/ui.R
@@ -4,21 +4,29 @@ shinyUI(
     navbarPage(
         "Construct Viewer",
         tabPanel("Network",
-                 h1("Construct Network Visualization"),
-                 p(paste("Here you will find all the constructs and their relations to one another.",
-                         sep = ' ')),
-                 p(paste("Hovering over a node will display the definition of the construct",
-                         sep = ' ')),
-                 p("Multiple edges refer to multiple definitions for the construct"),
-                 visNetworkOutput("vis_network", width = "100%", height = "800px"),
-                 p(paste(
-                     "You can click the edit button to modify and explore the network directly (without)",
-                     "changing the underlying data structure.",
-                     "For example, you can add an edge to/from 'Education' to 'Wisdom and Knowledge' and see",
-                     "how the networks change.",
-                     sep = ' '
-                 )),
-                 p("Double-cliking nodes will collapse all connections.")
+                 fluidPage(
+                     fluidRow(
+                         h1("Construct Network Visualization"),
+                         p(paste("Here you will find all the constructs and their relations to one another.",
+                                 sep = ' ')),
+                         p(paste("Hovering over a node will display the definition of the construct",
+                                 sep = ' ')),
+                         p("Multiple edges refer to multiple definitions for the construct")
+                     ),
+                     fluidRow(
+                         column(10,
+                                visNetworkOutput("vis_network", width = "100%", height = "800px")),
+                         column(2,
+                                p(paste(
+                                    "You can click the edit button to modify and explore the network directly (without)",
+                                    "changing the underlying data structure.",
+                                    "For example, you can add an edge to/from 'Education' to 'Wisdom and Knowledge' and see",
+                                    "how the networks change.",
+                                    sep = ' '
+                                )),
+                                p("Double-cliking nodes will collapse all connections."))
+                     )
+                 )
         ),
         tabPanel("Definitions",
                  h1("Construct Definitions"),
