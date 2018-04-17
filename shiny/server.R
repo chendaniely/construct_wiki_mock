edd3f01265b1bad508d5ea90c6820dd5e3b5b568
Daniel Chen <Maveri2k@yahoo.com>
2018-04-17 02:46:18 +0000

    construct/construct_values_network_dfs reactive

--- shiny/server.R
+++ shiny/server.R
@@ -12,23 +12,39 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
 
+    constructs <- reactive({
+        if (input$radio_data == ORIGINAL_DATA) {
+            read_delim(ORIGINAL_DATA,
+                       delim = '\t',
+                       skip = 1,
+                       col_names = c('timestamp', 'email', 'construct',
+                                     'meta_construct', 'definition', 'reference',
+                                     'bibtex', 'field', 'military', 'population',
+                                     'measurement', 'instrument', 'notes', 'bibkey'))
+        } else if (input$radio_data == ADJUSTED_DATA) {
+            read_delim(ADJUSTED_DATA,
+                       delim = '\t',
+                       skip = 1,
+                       col_names = c('timestamp', 'email', 'construct', 'meta_construct',
+                                     'meta_construct_ori', 'definition', 'reference',
+                                     'bibtex', 'field', 'military', 'population',
+                                     'measurement', 'instrument', 'notes', 'bibkey'))
+        }
+    })
 
     # pokemon <- read.csv('data/pokemon.csv', sep = '\t', stringsAsFactors = FALSE)
-    constructs <- read_delim('https://docs.google.com/spreadsheets/d/e/2PACX-1vR2imOgIzmv8ayb7rDrvVySZ0vFDGNULSGNgT5ObdXOyEEnrok-JlW4MP0jWNSJl1aP_UuKgVDsFZer/pub?gid=432933410&single=true&output=tsv',
-                             delim = '\t',
-                             skip = 1,
-                             #n_max = 81,
-                             col_names = c('timestamp', 'email', 'construct',
-                                           'meta_construct', 'definition', 'reference',
-                                           'bibtex', 'field', 'military', 'population',
-                                           'measurement', 'instrument', 'notes', 'bibkey'))
 
-    construct_values <- constructs$construct
-    names(construct_values) <- str_to_title(construct_values)
+    construct_values <- reactive({
+        cv <- constructs()$construct
+        names(cv) <- str_to_title(cv)
+        return(cv)
+    })
 
-    network_dfs <- .GlobalEnv$clean_network_df(constructs)
+    network_dfs <- reactive({
+        .GlobalEnv$clean_network_df(constructs())
+    })
 
-    G <- igraph::graph_from_data_frame(network_dfs$edges)
+    # G <- igraph::graph_from_data_frame(network_dfs$edges)
 
     # neighbors(G, 'character')
 
@@ -40,7 +56,7 @@ shinyServer(function(input, output, session) {
     #     visLayout(randomSeed = 42)
 
     output$vis_network <- renderVisNetwork({
-        visNetwork(network_dfs$nodes, network_dfs$edges, width = "100%", height = "75%") %>%
+        visNetwork(network_dfs()$nodes, network_dfs()$edges, width = "100%", height = "75%") %>%
             visEdges(arrows = "from") %>%
             visOptions(manipulation = TRUE,
                        collapse = TRUE) %>%
@@ -48,13 +64,13 @@ shinyServer(function(input, output, session) {
     })
 
     output$construction_selection <- renderUI({
-        selectizeInput('construct_name', 'Construct Name', sort(construct_values))
+        selectizeInput('construct_name', 'Construct Name', sort(construct_values()))
     })
 
     output$construct_definition <- renderUI({
         # md_text <- .GlobalEnv$parse_md_a('[A document](https://drive.google.com/open?id=0B7onm2yXv1-wX2FJVkxVUUZ3a2c)')
         # print(sprintf('selected: %s', input$construct_name))
-        construct_row_dat <- constructs[constructs$construct == input$construct_name, ]
+        construct_row_dat <- constructs()[constructs()$construct == input$construct_name, ]
 
         def_count_text <- if_else(nrow(construct_row_dat) == 1,
                                   sprintf("There is %s definition for this construct", nrow(construct_row_dat)),
@@ -65,9 +81,9 @@ shinyServer(function(input, output, session) {
     })
 
     output$construct_dt <- DT::renderDataTable({
-        constructs
-    }, options = list(pageLength = nrow(.GlobalEnv$constructs),
-                      lengthMenu = c(5, 10, 15, 20, 50, 100, nrow(.GlobalEnv$constructs)),
+        constructs()
+    }, options = list(pageLength = nrow(.GlobalEnv$constructs()),
+                      lengthMenu = c(5, 10, 15, 20, 50, 100, nrow(.GlobalEnv$constructs())),
                       colReorder = TRUE,
                       fixedHeader = TRUE,
                       extensions = 'Responsive'))
