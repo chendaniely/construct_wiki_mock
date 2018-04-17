b3a52c770a0c911c690c3db0b7a04a3c918781a1
Daniel Chen <Maveri2k@yahoo.com>
2018-04-17 01:55:59 +0000

    here and sourceDirectory

--- shiny/global.R
+++ shiny/global.R
@@ -6,15 +6,7 @@ library(dplyr)
 library(stringr)
 library(igraph)
 library(shinydashboard)
+library(here)
 
-if (interactive()) {
-    base_dir <- './'
-} else {
-    base_dir <- './'
-}
-
-fxns <- list.files(normalizePath(paste0(base_dir, 'functions')), full.names = TRUE)
-sapply(fxns, source, .GlobalEnv)
-
-mods <- list.files(normalizePath(paste0(base_dir, 'modules')), full.names = TRUE)
-sapply(mods, source, .GlobalEnv)
+print(R.utils::sourceDirectory(here('shiny', 'functions')))
+print(R.utils::sourceDirectory(here('shiny', 'modules')))
